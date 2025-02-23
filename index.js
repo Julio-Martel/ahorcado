4a256a79bfe41155b42ba743a78649ddd9dd3532
Diocleniax789 <julioramdom123@gmail.com>
2025-02-23 20:53:46 -0300

    solucionado el problema del modal

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const contenedorPrincipal = document.querySelector('.main-content');
 const botonStart = document.getElementById('main-button');
 const botonInstrucciones = document.getElementById('button-instrucciones');
 const ventanaModalInstrucciones = document.querySelector('.ventana-modal');
+const contenedorModal = document.querySelector('.contenido-modal');
+const botonCerrarModal = document.getElementById('boton-cerrar-modal');
 
 async function cambiarAContenidoJugable() {}
 
@@ -10,3 +12,7 @@ botonStart.addEventListener('click', cambiarAContenidoJugable);
 botonInstrucciones.addEventListener('click', () => ventanaModalInstrucciones.style.display = "flex" );
 
 ventanaModalInstrucciones.addEventListener('click', () => ventanaModalInstrucciones.style.display = "none");
+
+contenedorModal.addEventListener('click',() => event.stopPropagation());
+
+botonCerrarModal.addEventListener('click', () => ventanaModalInstrucciones.style.display = "none");
