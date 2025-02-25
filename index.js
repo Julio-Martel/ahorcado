5ca7683af209b6c6d89075d22f57ae215a0db3ee
Diocleniax789 <julioramdom123@gmail.com>
2025-02-25 00:08:04 -0300

    se ha realizado la variable con el nuevo contenido para el juego

--- index.js
+++ index.js
@@ -5,7 +5,77 @@ const ventanaModalInstrucciones = document.querySelector('.ventana-modal');
 const contenedorModal = document.querySelector('.contenido-modal');
 const botonCerrarModal = document.getElementById('boton-cerrar-modal');
 
-async function cambiarAContenidoJugable() {}
+async function cambiarAContenidoJugable() {
+	contenedorPrincipal.innerHTML = `
+		<div class="seccion-juego">
+			<div class="score-vidas">
+				<div class="contenido-estadistico" id="display-vidas">
+					<label for="intentos" name="intentos" class="intentos">Intentos:</label>
+					<div class="intentos-img">
+						<img src="images/imagesGame/icon-vida.png" class="icon-vida">
+						<img src="images/imagesGame/icon-vida.png" class="icon-vida">
+						<img src="images/imagesGame/icon-vida.png" class="icon-vida">
+						<img src="images/imagesGame/icon-vida.png" class="icon-vida">
+						<img src="images/imagesGame/icon-vida.png" class="icon-vida">
+					</div>
+				</div>
+				<div class="contenido-estadistico" id="display-score">
+					<label for="intentos" name="intentos" class="intentos">Score:</label>
+					<input type="number" value ="0" name="score" class="score-number" readonly>
+				</div>
+			</div>
+			<div class="display-muñeco">
+				<div class="contenedor-muñeco">
+				</div>
+			</div>
+
+			<div class="contenedor-palabra">
+			</div>
+
+			<div class="teclado-letras">
+				<div class="seccion-teclas">
+					<input type="button" value="a" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="b" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="c" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="d" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="e" name="letra" class="tecla-letra" readonly>
+				</div>
+				<div class="seccion-teclas">
+					<input type="button" value="f" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="g" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="h" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="i" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="j" name="letra" class="tecla-letra" readonly>
+				</div>
+				<div class="seccion-teclas">
+					<input type="button" value="k" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="m" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="n" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="ñ" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="l" name="letra" class="tecla-letra" readonly>
+				</div>
+				<div class="seccion-teclas">
+					<input type="button" value="o" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="p" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="q" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="r" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="s" name="letra" class="tecla-letra" readonly>
+				</div>
+				<div class="seccion-teclas">
+					<input type="button" value="t" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="u" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="v" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="w" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="x" name="letra" class="tecla-letra" readonly>
+				</div>
+				<div class="seccion-teclas">
+					<input type="button" value="y" name="letra" class="tecla-letra" readonly>
+					<input type="button" value="z" name="letra" class="tecla-letra" readonly>
+				</div>								
+			</div>
+		</div>
+	`;
+}
 
 botonStart.addEventListener('click', cambiarAContenidoJugable);
 
